6cdc0447754b7787308e2ebcba2973c7ec2d53c6
David Higgins <G00411302@gmit.ie>
2022-02-03 19:29:51 +0000

    adding testTypes.py file

---
+++ week03/lab3.1.X-testTypes.py
@@ -0,0 +1,14 @@
+#program to experiment with variables
+#Author: David Higgins
+
+i = 3       #this section assigns values to the variables
+fl = 3.5
+isa = True
+memo = 'how now brown cow'
+lots = []
+
+print('variable {} is of type: {} and value: {}'.format('i', type(i), i)) #this section uses format to place the variables in the string. type() is used to return the type of the variable
+print('variable {} is of type: {} and value: {}'.format('fl', type(fl), fl))
+print('variable {} is of type: {} and value: {}'.format('isa', type(isa), isa))
+print('variable {} is of type: {} and value: {}'.format('memo', type(memo), memo))
+print('variable {} is of type: {} and value: {}'.format('lots', type(lots), lots))
