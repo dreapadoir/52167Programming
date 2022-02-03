aac14571f71ba3307981018bccc289958ffa9765
David Higgins <G00411302@gmit.ie>
2022-02-03 19:34:31 +0000

    adding completed sub.py file

---
+++ week03/sub.py
@@ -0,0 +1,8 @@
+#program to subtract one number from another
+#Author: David Higgins
+
+x = int(input('Enter first number: '))
+y = int(input('Enter second number: '))
+answer = x-y
+
+print('{} minus {} is equal to {}'.format(x, y, answer))
