50d1bd6c261d33ca49072b4eefeb81cea157089d
David Higgins <G00411302@gmit.ie>
2022-02-09 19:38:23 +0000

    used math.ceil to handle rounding of numbers

--- week04/grades.py
+++ week04/grades.py
@@ -1,21 +1,22 @@
 #program to classify a percentage result as a grade
 #Author: David Higgins
 
-score = int(input("Enter your test score: "))
+import math
+score = float(input("Enter your test score: "))
 
 if score > 100 or score < 0:
     print("Score must be between 0 and 100")
 
-elif score < 40:
+elif math.ceil(score) < 40:
     print("Fail")
 
-elif score < 50:
+elif math.ceil(score )< 50:
     print("Pass")
 
-elif score < 60:
+elif math.ceil(score) < 60:
     print("Merit 1")
 
-elif score < 70:
+elif math.ceil(score) < 70:
     print("Merit 2")
 
 else:
