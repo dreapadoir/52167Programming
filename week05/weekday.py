5e70dc2458552c94c14abbeb818b541a70ce1229
David Higgins <G00411302@gmit.ie>
2022-02-19 18:03:55 +0000

    first version of week 5 task test program

---
+++ week05/weekday.py
@@ -0,0 +1,16 @@
+#program to test ideas for week 5 task
+#Author: David Higgins
+
+weekdays = ('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday')
+weekend = ('Saturday', 'Sunday')
+
+day = input("Enter a weekday: ")
+
+if day in weekdays:
+    print('It is a weekday')
+
+elif day in weekend:
+    print('It is the weekend')
+
+else:
+    print('Error: what you entered is not a day')
